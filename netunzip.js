4d3355c2694f46a066a4140de251042d59f83c47
Gergely Csucs <Tevemadar@users.noreply.github.com>
2023-09-14 13:59:10 +0200

    Zip64 partial support

--- netunzip.js
+++ netunzip.js
@@ -3,19 +3,28 @@ async function netunzip(locator) {
     const eodc = await fetch(await urlfunc(), {headers: {range: "bytes=-22"}}).then(response => response.arrayBuffer()).then(buffer => new DataView(buffer));
     if (eodc.getUint32(0, true) !== 0x06054b50)
         throw "0x06054b50, EODC header signature not found, file may not be a .zip or contains a comment field.";
-    const entries = new Map();
-    //console.log("Files",eodc.getUint16(10,true));
     const dirsize = eodc.getUint32(12, true);
-    const diroffset = eodc.getUint32(16, true);
-    //console.log(dirsize, diroffset);
+    let diroffset = eodc.getUint32(16, true);
+    if (diroffset === 0xFFFFFFFF) { //Zip64
+        const eocd64locator = await fetch(await urlfunc(), {headers: {range: "bytes=-42"}}).then(response => response.arrayBuffer()).then(buffer => new DataView(buffer));
+        if (eocd64locator.getUint32(0, true) !== 0x07064b50)
+            throw "0x07064b50, EODC64locator header signature not found, file may not be a .zip or contains a comment field.";
+        const eocd64offset = Number(eocd64locator.getBigUint64(8, true));
+        const eocd64 = await fetch(await urlfunc(), {headers: {range: `bytes=${eocd64offset}-`}}).then(response => response.arrayBuffer()).then(buffer => new DataView(buffer));
+        if (eocd64.getUint32(0, true) !== 0x06064b50)
+            throw "0x06064b50, EODC64 header signature not found, file may not be a .zip or contains a comment field.";
+        diroffset = Number(eocd64.getBigUint64(48, true));
+    }
     const dirbuf = await fetch(await urlfunc(), {headers: {range: `bytes=${diroffset}-${diroffset + dirsize - 1}`}}).then(response => response.arrayBuffer());
     let pos = 0;
     const tdec = new TextDecoder();
+    const entries = new Map();
     while (pos < dirbuf.byteLength) {
         const view = new DataView(dirbuf, pos);
         pos += 46;
-        if (view.getUint32(0, true) !== 0x02014b50)
+        if (view.getUint32(0, true) !== 0x02014b50) {
             throw "0x02014b50, directory entry signature not found, file may be damaged.";
+        }
         const timecode = view.getUint16(12, true);
         const hour = timecode >> 11;
         const minute = (timecode >> 5) & 63;
@@ -44,6 +53,35 @@ async function netunzip(locator) {
         entry.name = tdec.decode(new Uint8Array(dirbuf, pos, entry.namelength));
         pos += entry.namelength;
         entry.extra = new Uint8Array(dirbuf, pos, entry.extralength);
+        if (entry.offset === 0xFFFFFFFF) { // Zip64
+//            const extend = new Uint8Array(dirbuf, pos - 4, entry.extralength + 8);
+//            let nums = "";
+//            let chars = "";
+//            for (const b of extend) {
+//                nums += b.toString(16).padStart(2, "0") + " ";
+//                chars += b >= 32 && b < 127 ? String.fromCharCode(b) : ".";
+//                if (chars.length === 16) {
+//                    console.log(nums + chars);
+//                    nums = chars = "";
+//                }
+//            }
+//            if (nums !== "")
+//                console.log(nums.padEnd(3 * 16) + chars);
+
+            const extra = new DataView(dirbuf, pos, entry.extralength);
+            let epos = 0;
+            while (epos < extra.byteLength) {
+                const eid = extra.getUint16(epos, true);
+                const elen = extra.getUint16(epos + 2, true);
+                if (eid === 1) {
+                    if (elen !== 8)
+                        throw `Zip64 extra field length ${elen} is not supported yet.`;
+                    entry.offset = Number(extra.getBigUint64(epos + 4, true));
+                    break;
+                }
+                epos += elen + 4;
+            }
+        }
         pos += entry.extralength;
         entry.comment = tdec.decode(new Uint8Array(dirbuf, pos, entry.commentlength));
         pos += entry.commentlength;
@@ -71,4 +109,3 @@ async function netunzip(locator) {
         }
     };
 }
-
